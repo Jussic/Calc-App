bec7e1c049965e4553055fd215604dbc8c7e7b93
Jussic <justineclarkes@gmail.com>
2014-10-21 14:06:57 -0400

    Code
    Code for Calculator App, Controls

--- Calc App/ViewController.h
+++ Calc App/ViewController.h
@@ -8,6 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
+int method;
+int selectNum;
+float runningTotal;
+
 @interface ViewController : UIViewController
+
+{
+    IBOutlet UILabel *Screen;
+
+}
+
+    -(IBAction)num1:(id)sender;
+    -(IBAction)num2:(id)sender;
+    -(IBAction)num3:(id)sender;
+    -(IBAction)num4:(id)sender;
+    -(IBAction)num5:(id)sender;
+    
+    -(IBAction)num6:(id)sender;
+    -(IBAction)num7:(id)sender;
+    -(IBAction)num8:(id)sender;
+    -(IBAction)num9:(id)sender;
+    -(IBAction)num0:(id)sender;
+    
+    
+
+    -(IBAction)times:(id)sender;
+    -(IBAction)divide:(id)sender;
+    -(IBAction)sub:(id)sender;
+    -(IBAction)plus:(id)sender;
+    -(IBAction)equals:(id)sender;
+    -(IBAction)allclear:(id)sender;
 
 @end
--- Calc App/ViewController.m
+++ Calc App/ViewController.m
@@ -9,10 +9,275 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
+
+
+-(IBAction)num1:(id)sender{
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 1;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+    
+}
+-(IBAction)num2:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 2;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+    
+}
+-(IBAction)num3:(id)sender{
+    
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 3;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+    
+}
+-(IBAction)num4:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 4;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+-(IBAction)num5:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 5;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+    
+
+}
+
+
+
+-(IBAction)num6:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 6
+    ;
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+-(IBAction)num7:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 7;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+-(IBAction)num8:(id)sender{
+    
+    selectNum = selectNum * 10;
+    selectNum = selectNum + 8;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+-(IBAction)num9:(id)sender{
+    
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 9;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+-(IBAction)num0:(id)sender{
+    selectNum = selectNum * 10;
+        selectNum = selectNum + 0;
+    
+    Screen.text = [NSString stringWithFormat:@"%i", selectNum];
+}
+
+
+
+-(IBAction)times:(id)sender{
+    
+    if(runningTotal == 0)
+    {
+        runningTotal = selectNum;
+    
+    }
+    else{
+        switch (method)
+        {
+    case 1:
+        runningTotal = runningTotal * selectNum;
+        break;
+    case 2:
+        runningTotal = runningTotal / selectNum;
+        break;
+                
+    case 3:
+        runningTotal = runningTotal + selectNum;
+        break;
+    case 4:
+        runningTotal = runningTotal - selectNum;
+        break;
+    default:
+        break;
+        
+        }
+    
+    }
+    
+    method = 1;
+    selectNum = 0;
+}
+-(IBAction)divide:(id)sender{
+    
+    if(runningTotal == 0)
+    {
+        runningTotal = selectNum;
+        
+    }
+    else{
+        switch (method)
+        {
+            case 1:
+                runningTotal = runningTotal * selectNum;
+                break;
+            case 2:
+                runningTotal = runningTotal / selectNum;
+                break;
+                
+            case 3:
+                runningTotal = runningTotal + selectNum;
+                break;
+            case 4:
+                runningTotal = runningTotal - selectNum;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    
+    method = 2;
+    selectNum = 0;
+
+}
+-(IBAction)sub:(id)sender{
+    
+    if(runningTotal == 0)
+    {
+        runningTotal = selectNum;
+        
+    }
+    else{
+        switch (method)
+        {
+            case 1:
+                runningTotal = runningTotal * selectNum;
+                break;
+            case 2:
+                runningTotal = runningTotal / selectNum;
+                break;
+                
+            case 3:
+                runningTotal = runningTotal + selectNum;
+                break;
+            case 4:
+                runningTotal = runningTotal - selectNum;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    
+    method = 3;
+    selectNum = 0;
+
+    
+}
+-(IBAction)plus:(id)sender{
+    
+    if(runningTotal == 0)
+    {
+        runningTotal = selectNum;
+        
+    }
+    else{
+        switch (method)
+        {
+            case 1:
+                runningTotal = runningTotal * selectNum;
+                break;
+            case 2:
+                runningTotal = runningTotal / selectNum;
+                break;
+                
+            case 3:
+                runningTotal = runningTotal + selectNum;
+                break;
+            case 4:
+                runningTotal = runningTotal - selectNum;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    
+    method = 4;
+    selectNum = 0;
+
+
+}
+
+
+-(IBAction)equals:(id)sender{
+
+    if(runningTotal == 0)
+    {
+        runningTotal = selectNum;
+        
+    }
+    else{
+        switch (method)
+        {
+            case 1:
+                runningTotal = runningTotal * selectNum;
+                break;
+            case 2:
+                runningTotal = runningTotal / selectNum;
+                break;
+                
+            case 3:
+                runningTotal = runningTotal + selectNum;
+                break;
+            case 4:
+                runningTotal = runningTotal - selectNum;
+                break;
+            default:
+                break;
+                
+        }
+        
+    }
+    
+    method = 0;
+    selectNum = 0;
+    Screen.text = [NSString stringWithFormat:@"%.2f", runningTotal];
+}
+
+
+-(IBAction)allclear:(id)sender{
+
+    method = 0;
+    runningTotal = 0;
+    selectNum = 0;
+    
+    Screen.text = [NSString stringWithFormat:@"0"];
+}
+
 
 - (void)viewDidLoad
 {
